cfc73834682434aeb3a7fd761aab1cfa8908db41
Alex X Xu <xualexander1@gmail.com>
2018-11-27 22:36:00 -0800

    Added two views

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from "./containers/login";
 import Create from "./containers/create";
-
+import Camera from "./containers/camera";
+import Pay from "./containers/pay";
 
 import logo from './logo.svg';
 import './App.css';
@@ -14,6 +15,8 @@ class App extends Component {
         <div className="App">
           <Route path="/" exact component={Login} />
           <Route path="/create/" component={Create} />
+          <Route path="/camera/" exact component={Camera} />
+          <Route path="/pay/" component={Pay} />
         </div>
       </Router>
     );
---
+++ src/containers/camera.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Camera extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (<div></div>);
+  }
+}
+
+
+export default Camera;
