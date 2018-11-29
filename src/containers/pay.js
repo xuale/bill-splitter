a11ef73e9dd6a65849ddfe59cb4af6806b56e367
Anshul Aggarwal <agg_anshul@yahoo.com>
2018-11-29 15:16:28 -0800

    setting the receipt up

--- src/containers/pay.js
+++ src/containers/pay.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import linkState from 'linkstate';
+import { Button, Row, Card, Col, Input } from 'antd';
+
+import UserService from '../backend/services/users';
 
 class Pay extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (<div></div>);
-  }
-}
+	constructor(props) {
+		super(props);
+		this.state = {
+			friends: [],
+			total: [],
+			firstName: '',
+			lastName: '',
+			isCompleted: false
+		};
+	}
 
+	render() {
+		const { isCompleted } = this.state;
+		return (
+			<Row style={{marginTop: '4rem'}}>
+				<Col span={8} offset={8}>
+					{isCompleted && (
+						<p></p>
+					)}
+					{!isCompleted && (
+						<Card title="Pay">
+							<p style={{margin: '12px', textAlign: 'left'}}>
+								Receipt:
+							</p>
+						</Card>
+					)}
+				</Col>
+			</Row>
+		);
+	}
+}
 
 export default Pay;
