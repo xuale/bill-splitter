a77f92535b5672b9736ad94dc85d074ec3681042
Alex X Xu <xualexander1@gmail.com>
2018-11-28 21:51:00 -0800

    UI

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,38 +1,90 @@
 import React, { Component } from 'react';
 import linkState from 'linkstate';
+import { Button, Row, Card, Col, Input } from 'antd';
 
 import UserService from '../backend/services/users';
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      first_name: '',
-      last_name: '',
-      isNewAccount: false
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+			firstName: '',
+			lastName: '',
+			isNewAccount: false
+		};
+
+    this.switchLoginRegister = this.switchLoginRegister.bind(this);
+    this.register = this.register.bind(this);
+    this.login = this.login.bind(this);
+	}
+
+	switchLoginRegister() {
+		const { isNewAccount } = this.state;
+		this.setState({
+			isNewAccount: !isNewAccount
+		});
+  }
+  
+  async register() {
+		await UserService.register(this.state);
+		this.login();
   }
 
-  render() {
-    const { username, password, isNewAccount } = this.state;
-    return (
-      <div>
-        <h1>Split.io</h1>
-        {isNewAccount && 
-          <div>
-            <h3>Register</h3>
-            <input type="text" onInput={linkState(this, 'email')} placeholder="Email"/>
-          </div>
-        }
-        {!isNewAccount &&
-          <div></div>
-        }
-      </div>
-    );
+  async login() {
+		const user = await UserService.login(this.state);
+		console.log(user);
+    this.saveLoginLocally(user);
   }
+
+  saveLoginLocally(user) {
+		const { id, firstName } = user;
+
+    localStorage.setItem("isLoggedIn", "true");
+		localStorage.setItem("id", id);
+		localStorage.setItem("firstName", firstName);
+
+    this.enterCreate();
+  }
+
+  enterCreate() {
+    this.props.history.push('/create');
+  }
+
+	render() {
+		const { isNewAccount } = this.state;
+		return (
+			<Row style={{marginTop: '4rem'}}>
+				<Col span={8} offset={8}>
+					{isNewAccount && (
+						<Card title="Register">
+							<Input placeholder="First Name" onInput={linkState(this, 'firstName')} style={{margin: '8px'}}/>
+              <Input placeholder="Last Name" onInput={linkState(this, 'lastName')} style={{margin: '8px'}}/>
+              <Input placeholder="Email" onInput={linkState(this, 'email')} style={{margin: '8px'}}/>
+              <Input placeholder="Password" type="password" onInput={linkState(this, 'password')} style={{margin: '8px'}}/>
+              <Input placeholder="Confirm Password" type="password" style={{margin: '8px'}}/>
+              <Button type="primary" onClick={this.register} style={{margin: '8px'}}>Create Account</Button>
+
+							<p style={{margin: '8px'}}>
+								Already have an account? <a onClick={this.switchLoginRegister}>Click here to login. </a>
+							</p>
+						</Card>
+					)}
+					{!isNewAccount && (
+						<Card title="Login">
+							<Input placeholder="Email" onInput={linkState(this, 'email')} style={{margin: '8px'}}/>
+              <Input placeholder="Password" type="password" onInput={linkState(this, 'password')} style={{margin: '8px'}}/>
+              <Button type="primary" onClick={this.login} style={{margin: '8px'}}>Login</Button>
+							<p style={{margin: '8px'}}>
+								Don't have an account? <a onClick={this.switchLoginRegister}>Click here to register.</a>
+							</p>
+						</Card>
+					)}
+				</Col>
+			</Row>
+		);
+	}
 }
-
 
 export default Login;
