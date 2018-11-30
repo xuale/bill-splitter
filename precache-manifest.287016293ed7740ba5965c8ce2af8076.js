fa97a32829bb578c8301e2dabad34721c2937ba1
Alex X Xu <xualexander1@gmail.com>
2018-11-30 01:12:09 -0800

    Updates

---
+++ precache-manifest.287016293ed7740ba5965c8ce2af8076.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "76d72c76f84aea8ea182",
+    "url": "/bill-splitter/static/css/main.c6727286.chunk.css"
+  },
+  {
+    "revision": "76d72c76f84aea8ea182",
+    "url": "/bill-splitter/static/js/main.76d72c76.chunk.js"
+  },
+  {
+    "revision": "d3766b97a84fa53f1295",
+    "url": "/bill-splitter/static/css/1.29a472df.chunk.css"
+  },
+  {
+    "revision": "d3766b97a84fa53f1295",
+    "url": "/bill-splitter/static/js/1.d3766b97.chunk.js"
+  },
+  {
+    "revision": "d84661d649d93b4eb455",
+    "url": "/bill-splitter/static/js/runtime~main.d84661d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bill-splitter/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f6b33cd22663cd1659985f8f78d74365",
+    "url": "/bill-splitter/index.html"
+  }
+];
