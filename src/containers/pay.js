ab92629beea63fabb969e2a7c373a564e72a21c6
Alex X Xu <xualexander1@gmail.com>
2018-11-29 20:00:43 -0800

    pay fix

--- src/containers/pay.js
+++ src/containers/pay.js
@@ -9,55 +9,73 @@ class Pay extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			friends: ["alice", "bob", "lit"],
+			friends: [ 'alice', 'bob', 'lit' ],
 			total: [],
 			firstName: 'Anshul',
 			lastName: 'Aggarwal',
 			isCompleted: false
-    };
-    this.clickForPayment = this.clickForPayment.bind(this);
-    this.getName = this.getName.bind(this)
+		};
+		this.clickForPayment = this.clickForPayment.bind(this);
+		this.getName = this.getName.bind(this);
 	}
 
-  clickForPayment() {
+	componentDidMount() {
+		const firstName = localStorage.getItem("firstName");
+		const lastName = localStorage.getItem("lastName");
+		this.setState({
+			firstName, lastName
+		})
+	}
+
+	clickForPayment() {
 		const { isCompleted } = this.state;
 		this.setState({
 			isCompleted: !isCompleted
 		});
-  }
+	}
 
-  getName() {
-    return this.state.firstName + " " + this.state.lastName;
-  }
+	getName() {
+		return this.state.firstName + ' ' + this.state.lastName;
+	}
 
 	render() {
-    const { friends, total, firstName, lastName, isCompleted } = this.state;
+		const { friends, total, firstName, lastName, isCompleted } = this.state;
 		return (
-			<Row style={{marginTop: '4rem'}}>
+			<Row style={{ marginTop: '4rem' }}>
 				<Col span={8} offset={8}>
 					{isCompleted && (
-            <Card>
-						<p>Success</p>
-            <p><Button type="primary" onClick={this.clickForPayment} style={{margin: '8px'}}>Send Payments</Button></p>
-            </Card>
+						<Card>
+							<p>Success</p>
+							<p>
+								<Button type="primary" onClick={this.clickForPayment} style={{ margin: '8px' }}>
+									Send Payments
+								</Button>
+							</p>
+						</Card>
 					)}
 					{!isCompleted && (
 						<Card title="Pay">
-							<p style={{margin: '12px', textAlign: 'left'}}>
-                Receipt: { this.getName() }
-							</p>
-              <Col>
-                {friends.map(function(friend, i) {
-                        return (
-                          <Row>
-                            <Col span={12}>{friend}</Col> <Col span={12}><Input placeholder="Enter Value To Pay" style={{margin: '12px', display: 'inline'}}/></Col>	
-                          </Row>
-                        );
-                    })}
+							<p style={{ margin: '12px', textAlign: 'left' }}>Receipt: {this.getName()}</p>
+							<Col>
+								{friends.map(function(friend, i) {
+									return (
+										<Row>
+											<Col span={12}>{friend}</Col>{' '}
+											<Col span={12}>
+												<Input
+													placeholder="Enter Value To Pay"
+													style={{ margin: '12px', display: 'inline' }}
+												/>
+											</Col>
+										</Row>
+									);
+								})}
 							</Col>
-              <p style={{margin: '12px', textAlign: 'right'}}>
-                <Button type="primary" onClick={this.clickForPayment} style={{margin: '8px'}}>Send Payments</Button>
-              </p>
+							<p style={{ margin: '12px', textAlign: 'right' }}>
+								<Button type="primary" onClick={this.clickForPayment} style={{ margin: '8px' }}>
+									Send Payments
+								</Button>
+							</p>
 						</Card>
 					)}
 				</Col>
